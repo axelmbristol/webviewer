fbcf20ba7ad3020dd1398fd85b9c949df4f04c79
Axel <axelmontout@hotmail.fr>
2018-11-02 04:46:37 +0000

    can plot activity level for 1 animal from mongoDB.

--- src/app.py
+++ src/app.py
@@ -4,39 +4,78 @@ import dash_core_components as dcc
 import dash_html_components as html
 import numpy as np
 import plotly.graph_objs as go
+import pymongo
+from pymongo import MongoClient
+import datetime
 
+print('init mongoDB...')
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+db = client['70101100029']
+
+colNames = db.list_collection_names()
+
+date = []
+activity = []
+cpt = 0
+colNames.sort()
+for name in colNames:
+    print(str(cpt)+"/"+str(len(colNames))+" "+name + "...")
+    collection = db[name]
+
+    animals = collection.find_one()["animals"]
+
+    for n in animals:
+        if n["serial_number"] == 40061201116:
+            animal = n
+            tag_data = animal["tag_data"]
+            serial = tag_data[0]["serial_number"]
+            for entry in tag_data:
+                a = entry["first_sensor_value"]
+                if a > 1000:
+                    a = 1000
+                if a < 0:
+                    a = 0
+
+                activity.append(a)
+                formated = datetime.datetime.strptime(entry["date"] + " " + entry["time"], '%d/%m/%y %I:%M:%S %p').strftime(
+                    '%Y-%m-%dT%H:%M')
+                date.append(formated)
+
+                #print(str(a) + " " + str(formated))
+
+    cpt = cpt + 1
+    if cpt > 7:
+        break
+
+print('init dash...')
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-N = 500
-_y = np.array([0,1,2,3])
-_x = np.array([np.datetime64('2005-02-25T03:30'),
-               np.datetime64('2005-02-25T03:31'),
-               np.datetime64('2005-02-25T03:32'),
-               np.datetime64('2005-02-25T03:33')])
-
 app.layout = html.Div(children=[
-    html.H1(children='farmId'),
+    html.H1(children=serial),
 
     html.Div(children='''
-        description.
+        activity level over time.
     '''),
 
     dcc.Graph(
         figure=go.Figure(
             data=[
                 go.Scatter(
-                    x=_x,
-                    y=_y,
-                    name='animal1',
+                    x=date,
+                    y=activity,
+                    name=serial,
                 )
-            ]
+            ],
+            layout=go.Layout(
+                margin=go.layout.Margin(l=40, r=0, t=40, b=160)
+            )
         ),
-        style={'height': 300},
+        style={'height': '80vh'},
         id='my-graph'
     )
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, use_reloader=False)
